145420666624bdf98a0994aef8ff0322d949ed5f
SkareMartin22 <815360@mystma.org>
2021-12-15 09:13:15 -0600

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a,b,c,d,e):
+    return a+b+c+d+e
+
+print("Spiderman Into the Spiderverse", " Monty Python and the Holy Gail ", "The Dark Knight Rises ", "1917 ", "Whiplash ")
