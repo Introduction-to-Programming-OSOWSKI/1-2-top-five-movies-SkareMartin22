31cd3018e2beb45074714078f4d9d3f8325a83b0
SkareMartin22 <815360@mystma.org>
2021-12-15 09:21:24 -0600

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a,b,c,d,e):
-    return a+b+c+d+e
+    return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMovies("Spiderman Into the Spiderverse", " Monty Python and the Holy Gail ", "The Dark Knight Rises ", "1917 ", "Whiplash "))
+print(topFiveMovies("Spiderman Into the Spiderverse", "Monty Python and the Holy Gail", "The Dark Knight Rises", "1917", "Whiplash"))
